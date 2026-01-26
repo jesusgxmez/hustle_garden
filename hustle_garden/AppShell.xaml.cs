38fcc699f9179198636ee6378622129590d71747
tjerito <fernandetejeroivan@gmail.com>
2026-01-26 14:27:11 +0100

    Pantalla agregar plantas corregida

--- hustle_garden/AppShell.xaml.cs
+++ hustle_garden/AppShell.xaml.cs
@@ -9,6 +9,28 @@ namespace hustle_garden
             InitializeComponent();
             
             Routing.RegisterRoute("DetallePlantaPage", typeof(DetallePlantaPage));
+            
+            // Escuchar cuando cambia la pestaña seleccionada
+            this.PropertyChanged += OnShellPropertyChanged;
+        }
+
+        private async void OnShellPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            // Solo actuar cuando cambia la pestaña actual
+            if (e.PropertyName == nameof(CurrentItem))
+            {
+                // Solo hacer Pop si hay páginas en la pila
+                if (Navigation.NavigationStack.Count > 1)
+                {
+                    await Navigation.PopAsync();
+                }
+            }
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            this.PropertyChanged -= OnShellPropertyChanged;
         }
     }
 }
